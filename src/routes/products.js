105cd81d0aa51246d293e694a8f83423c0f2e72f
faryadicka <faryadicka98@gmail.com>
2022-04-26 17:20:59 +0700

    Add new route for Detail Product

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,14 +5,13 @@ const {
   insertProductControl,
   updateProductControl,
   deleteProductControl,
+  getProductDetailControl,
 } = require("../controllers/products")
 
-// const {
-//   queryFind
-// } = require("../middlewares/validation")
 
 Router
-  .get("/all", getProductsControl)
+  .get("/search", getProductsControl)
+  .get("/detail/:id", getProductDetailControl)
   .post("/insert", insertProductControl)
   .patch("/update/:id", updateProductControl)
   .delete("/delete/:id", deleteProductControl)
