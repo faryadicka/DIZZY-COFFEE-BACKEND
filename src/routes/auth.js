1c83e05047a1e11b91147dd39569390e339957ed
faryadicka <faryadicka98@gmail.com>
2022-05-11 12:37:47 +0700

    ADD NEW ROUTE FOR AUTH

---
+++ src/routes/auth.js
@@ -0,0 +1,7 @@
+const Router = require("express").Router()
+const {registerUserControl, loginUserControl} = require("../controllers/auth")
+Router
+  .post("/register", registerUserControl)
+  .post("/login", loginUserControl)
+
+module.exports = Router
