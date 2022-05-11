858c213e00a2609dc8ae4eb97a9bdef7e3cd606e
faryadicka <faryadicka98@gmail.com>
2022-05-11 12:38:21 +0700

    ADD NEW ROUTE FOR AUTH

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const productsRouter = require("./products")
 const usersRouter = require("./users")
 const promosRouter = require("./promos")
 const transactionRoute = require("./transactions")
+const authRouter = require("../routes/auth")
 
 
 Router
   .use("/products", productsRouter)
   .use("/users", usersRouter)
+  .use("/auth", authRouter)
   .use("/promos", promosRouter)
   .use("/transactions", transactionRoute)
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -7,18 +7,11 @@ const {
 } = require("../controllers/users")
 
 const {
-  registerUserControl,
-  loginUserControl
-} = require("../controllers/auth")
-
-const {
   InsertUser
 } = require("../middlewares/validation")
 Router
 
 Router
-  .post("/register", registerUserControl)
-  .post("/login", loginUserControl)
   .post("/", InsertUser, insertUserControl)
   .get("/", getAllUsersControl)
   .get("/:id", getDetailUserControl)
