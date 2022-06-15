3c678eee17a6a6fb6e24edec42bb8ca4d08bbb88
faryadicka <faryadicka98@gmail.com>
2022-06-15 16:24:12 +0700

    CHANGE PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const cors = require("cors")
 const db = require("./src/config/db")
 const mainRoute = require("./src/routes/index")
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 
 db.connect()
