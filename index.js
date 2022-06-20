70ae25fcdb4c04d0c0ab1efde99cfe5cf815b652
faryadicka <faryadicka98@gmail.com>
2022-06-21 01:36:19 +0700

    fix CORS

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ db.connect()
     const corsOptions = {
       origin: ["http://192.168.43.191:3000", "http://localhost:3000", "https://dizzycoffeeshop.netlify.app"],
       methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS", "PUT"],
-      allowedHeaders: ["content-type", "x-access-token"],
     }
     app.use(cors(corsOptions))
 
