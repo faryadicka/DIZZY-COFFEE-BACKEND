62cc0b65a18e16c45cc8e24c4b19c5799159af55
faryadicka <faryadicka98@gmail.com>
2022-06-21 01:25:07 +0700

    configure CORS

--- index.js
+++ index.js
@@ -32,9 +32,7 @@ db.connect()
     //CORS
     // install CORS
     const corsOptions = {
-      origin: ["*", "http://192.168.43.191:3000", "http://localhost:3000", "https://dizzycoffeeshop.netlify.app"],
-      methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS", "PUT"],
-      allowedHeaders: ["content-type", "x-access-token"],
+      origin: "http://localhost:3000"
     }
     app.use(cors(corsOptions))
 
