c3d46e4ee1c42704602de856e3ca2a1bd92c4566
faryadicka <faryadicka98@gmail.com>
2022-06-20 21:09:40 +0700

    FIX CONFIG CLOUDINARY

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -3,8 +3,8 @@ const cloudinary = require("cloudinary").v2
 const cloudConfig = (req, res, next) => {
   cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_KEY,
-    api_secret: process.env.CLOUD_SECRET,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
   });
   next();
 };
