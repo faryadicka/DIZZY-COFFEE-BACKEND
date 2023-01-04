c9035a25efb207ed8a5ff0c41f9a95ceab576fad
Ferry Aryadicka <93524589+faryadicka@users.noreply.github.com>
2023-01-04 14:49:14 +0700

    delete comma

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ const database = new Pool({
     process.env.DATABASE_URL ||
     "postgres://nwqnpoitrzemiq:388a83c291ef1f939a132a7b42102bcca6a33164a1feb53609701596d4a5c622@ec2-54-145-204-49.compute-1.amazonaws.com:5432/d55jqri3nobb2o",
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false
   },
 });
 
